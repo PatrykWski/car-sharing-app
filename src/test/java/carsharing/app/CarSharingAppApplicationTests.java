e4312a5bcff5c983e95de0ea809c6ebed2c67817
PatrykWski <patrykwiszniewski96@gmail.com>
2026-07-31 15:56:52 +0200

    Initial commit - structure and pom.xml setup

---
+++ src/test/java/carsharing/app/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package carsharing.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/sys/app/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sys.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
