075475e6ebd97fe70403308f98e8c59957dbd64c
PatrykWski <patrykwiszniewski96@gmail.com>
2026-08-07 16:51:27 +0200

    Edit test properties and write fake keys

--- src/test/java/carsharing/app/CarSharingAppApplicationTests.java
+++ src/test/java/carsharing/app/CarSharingAppApplicationTests.java
@@ -2,10 +2,8 @@ package carsharing.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "file:.env")
 class CarSharingAppApplicationTests {
 
     @Test
