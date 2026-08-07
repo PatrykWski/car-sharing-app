12a1c53b6a311045c3660e53f90a7f1cb64233aa
PatrykWski <patrykwiszniewski96@gmail.com>
2026-08-07 16:40:50 +0200

    Create .env file and .exampleENV file, turn on CarSharingAppApplicationTests.class

--- src/main/java/carsharing/app/AdminInitializer.java
+++ src/main/java/carsharing/app/AdminInitializer.java
@@ -1,4 +1,36 @@
 package carsharing.app;
 
-public class AdminInitializer {
+import carsharing.app.model.RoleName;
+import carsharing.app.model.User;
+import carsharing.app.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${app.manager.email}")
+    private String email;
+
+    @Value("${app.manager.password}")
+    private String rawPassword;
+
+    @Override
+    public void run(String... args) {
+        if (userRepository.findByEmail(email).isEmpty()) {
+            User manager = new User();
+            manager.setEmail(email);
+            manager.setPassword(passwordEncoder.encode(rawPassword));
+            manager.setRoleName(RoleName.MANAGER);
+            manager.setFirstName("System");
+            manager.setLastName("Manager");
+            userRepository.save(manager);
+        }
+    }
 }
--- src/test/java/carsharing/app/CarSharingAppApplicationTests.java
+++ src/test/java/carsharing/app/CarSharingAppApplicationTests.java
@@ -1,11 +1,14 @@
 package carsharing.app;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "file:.env")
 class CarSharingAppApplicationTests {
 
-    //@Test
-    //void contextLoads() {
-    //}
+    @Test
+    void contextLoads() {
+    }
 }
